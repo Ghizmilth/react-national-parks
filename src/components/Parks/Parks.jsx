27891c8145b2866c7cdf5e2a1dcb2551ac3702bb
Hidaner Ferrer <hidanerferrer@gmail.com>
2021-04-05 11:33:37 -0700

    Add a Grid to display parks

--- src/components/Parks/Parks.jsx
+++ src/components/Parks/Parks.jsx
@@ -1,23 +1,58 @@
 import React, { useState } from 'react';
 import data from '../../api/data.json';
 
+import { useStyles } from './ParksStyles.js';
+
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+import Container from '@material-ui/core/Container';
+
+
+
+const styles = useStyles;
+
+const parkTiles =
+    <div className={styles.root}> 
+    <Grid container spacing={2}>
+       { 
+           data.map(park => {
+                return (   
+                   <Grid item xs={4} sm={3}>
+                        <Paper className={styles.paper}><p><strong>{ park.fullName }</strong></p></Paper>
+                    </Grid>
+            )
+        })
+       }
+    </Grid>
+    </div>;
+
+// const parkGrid = <div className={styles.root}>
+// <Grid container spacing={2}>
+//   <Grid item xs={4} sm={3}>
+//     <Paper className={styles.paper}><p><strong>{ park.fullName }</strong></p></Paper>
+//   </Grid>
+//   <Grid item xs={4} sm={3}>
+//     <Paper className={styles.paper}>xs=6 sm=3</Paper>
+//   </Grid>
+//   <Grid item xs={4} sm={3}>
+//     <Paper className={styles.paper}>xs=6 sm=3</Paper>
+//   </Grid>
+//   <Grid item xs={4} sm={3}>
+//     <Paper className={styles.paper}>xs=6 sm=3</Paper>
+//   </Grid>
+// </Grid>
+// </div>;
+
 
 const Parks = () => {
 
     return (
         <div>
-            <ul>
-            {data.map(park => {
-                return (
-                    <li>
-                       <p><strong>{ park.fullName }</strong></p>
-                    </li>
-                )
-            }
-            )
-            } 
-            </ul>
+            <Container>
+                <div>{ parkTiles }</div>
+            </Container>
         </div>
+        
     )
 }
 
